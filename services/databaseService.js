63138a599bd982c5a694f5f86556827efae5d035
Michael Prattinger <mprattinger@outlook.com>
2017-11-09 22:24:31 +0100

    NeDb Setup + Database Service

---
+++ services/databaseService.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const winston = require("winston");
+const events = require("events");
+const path = require("path");
+const nedb = require("nedb");
+
+class DatabaseService {
+
+    constructor(){
+
+    }
+}
